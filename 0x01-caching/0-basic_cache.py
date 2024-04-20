83b6639740fb1fd1ab4b9cccb5c8cbe47fc28218
israel-09 <israelinene91@gmail.com>
2024-04-20 17:50:35 +0100

    task 0 solution

---
+++ 0x01-caching/0-basic_cache.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+"""
+base cache module
+"""
+from base_cache import BaseCaching
+
+
+class BasicCache(BaseCaching):
+    """
+    implement the put and get methods
+    """
+    def put(self, key, item):
+        """
+        add an item to the cache
+        """
+        if key is not None and item is not None:
+            self.cache_data[key] = item
+
+    def get(self, key):
+        """
+        retrieve the value of a key
+        """
+        if key is not None:
+            return self.cache_data.get(key)
